14dca8a5ac763cbb7d2306af105d3d26859e16ad
faishalfhid <faishalfh1@gmail.com>
2024-05-16 17:28:45 +0700

    adding login page

--- app/src/main/java/com/dicoding/suargaapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/suargaapp/ui/login/LoginActivity.kt
@@ -1,8 +1,18 @@
 package com.dicoding.suargaapp.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.dicoding.suargaapp.R
 import com.dicoding.suargaapp.databinding.ActivityLoginBinding
 import com.dicoding.suargaapp.databinding.ActivityMainBinding
@@ -24,5 +34,33 @@ class LoginActivity : AppCompatActivity() {
 
     private fun setupAction(){
 
+        setSupportActionBar(binding.topAppBar)
+        binding.topAppBar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        val textView = binding.forgotPass
+        val text = "Lupa Password?"
+        val spannableString = SpannableString(text)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Toast.makeText(this@LoginActivity, "Halaman ganti password", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+                ds.color = ContextCompat.getColor(this@LoginActivity, R.color.primary60)
+            }
+        }
+
+        val start = text.indexOf("Lupa Password?")
+        val end = start + "Lupa Password?".length
+        spannableString.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(ContextCompat.getColor(this, R.color.primary80)), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
